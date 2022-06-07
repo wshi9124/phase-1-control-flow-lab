b7dcaf739033b3e9e94c3933608f4e5579aadeb8
Willie Shi <wshi9124@gmail.com>
2022-06-07 16:42:51 -0400

    commit tree 2239354d8646f72414d1744b0bc565391375ab75

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if (ride <= 400) {
+    return 'This one is on me!'
+  }
+
+  else if (ride <= 2000) {
+    return 'That will be twenty bucks.'
+  }
+  
+  else if (ride > 2000 && ride < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+
+  if (ride > 2500) {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city){
+  return (city= "NYC" ? "Ok, sounds good." :"No go.")
 }
 
 function switchOnCharmFromTip(){
